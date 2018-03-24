7386e809752d74462f90999a2db6de80297d82a3
Thomas LION <tahitibob035@gmail.com>
2018-03-25 00:25:00 +0100

    finalize with http server

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const SteamAPI = require('./lib/SteamAPI')
 const config = require('./lib/config')
-const steamAPI = new SteamAPI(config)
-console.log('config:', config)
-steamAPI.run()
+const http = require('http');
+
+try {
+    const steamAPI = new SteamAPI(config)
+    steamAPI.run()    
+
+    http.createServer(async function (req, res) {
+        try {
+            res.writeHead(200, {'Content-Type': 'application/json'})
+            res.write(await steamAPI._getContentPrevResult())    
+        } catch (error) {
+            res.writeHead(501, {'Content-Type': 'text/plain; charset=utf-8'})
+            console.log(`SERVER ERROR !! ${error.message}`)
+            res.write(`SERVER ERROR !!`)
+        }
+        res.end()
+    }).listen(8080)
+    
+} catch (error) {
+    console.log('error:', error)
+}
+
